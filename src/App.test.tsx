ba54b6b2048466fd7e8b40ab352c448485c1e482
jtutzo <jeremytutzo@gmail.com>
2020-05-29 12:24:38 +0200

    Test routes

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,20 +1,61 @@
 import React from 'react';
 import {render, RenderResult} from '@testing-library/react';
 import App from './App';
+import { MemoryRouter } from 'react-router-dom';
 
 jest.mock('./components/Home', () => () => <div data-testid={'home'}>Home</div>)
 jest.mock('./components/Article', () => () => <div data-testid={'article'}>Article</div>)
+jest.mock('./components/NotFound', () => () => <div data-testid={'notFound'}>notFound</div>)
 
 describe('App component', () => {
-
     let app: RenderResult
 
-    beforeEach(() => {
-        app = component()
+    describe('path as /', () => {
+        beforeEach(() => {
+            app = component('/')
+        })
+        test('should display home page', () => {
+            expect(app.queryByTestId('home')).not.toBeNull()
+        })
+        test('should not display article page', () => {
+            expect(app.queryByTestId('article')).toBeNull()
+        })
+        test('should not display not found page', () => {
+            expect(app.queryByTestId('notFound')).toBeNull()
+        })
     })
-    test('should display home', () => {
-        expect(app.queryByTestId('home')).not.toBeNull()
+    describe('path as /article/2', () => {
+        beforeEach(() => {
+            app = component('/article/2')
+        })
+        test('should not display home page', () => {
+            expect(app.queryByTestId('home')).toBeNull()
+        })
+        test('should display article page', () => {
+            expect(app.queryByTestId('article')).not.toBeNull()
+        })
+        test('should not display not found page', () => {
+            expect(app.queryByTestId('notFound')).toBeNull()
+        })
+    })
+    describe('path as /unknown', () => {
+        beforeEach(() => {
+            app = component('/unknown')
+        })
+        test('should not display home page', () => {
+            expect(app.queryByTestId('home')).toBeNull()
+        })
+        test('should not display article page', () => {
+            expect(app.queryByTestId('article')).toBeNull()
+        })
+        test('should display not found page', () => {
+            expect(app.queryByTestId('notFound')).not.toBeNull()
+        })
     })
 })
 
-const component = () => render(<App/>)
+const component = (path = '/') => render(
+    <MemoryRouter initialEntries={[path]}>
+        <App/>
+    </MemoryRouter>
+    )
--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from "./components/Home";
 import {Provider} from "react-redux";
 import store from "./store/configStore";
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
-import {Navbar} from 'react-bootstrap';
+import {Route, Switch} from 'react-router-dom';
 import Article from "./components/Article";
+import NotFound from "./components/NotFound";
 
 const App: React.FC = () => {
     return (
         <div className="App">
             <Provider store={store}>
-                <BrowserRouter>
-                    <Switch>
-                        <Route exact path={'/article/:id'} component={Article} />
-                        <Route path={'/'} component={Home} />
-                    </Switch>
-                </BrowserRouter>
+                <Switch>
+                    <Route exact path={'/article/:id'} component={Article}/>
+                    <Route exact path={'/'} component={Home}/>
+                    <Route path={'*'} component={NotFound}/>
+                </Switch>
             </Provider>
         </div>
     );
